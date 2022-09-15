43842641d2d30e786451a43aa28ad449a6b91326
Timothy G. Flynn <tgflynn@users.noreply.github.com>
2022-09-15 08:32:16 -0400

    Initial checkin

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+const canvasWidth = 100;
+const canvasHeight = 100;
+
+const { ctx } = VBCanvas.createCanvas({
+    // viewBox (x, y, width, height)
+    viewBox: [0, 0, canvasWidth, canvasHeight],
+    // where to mount the <canvas> element
+    target: '.canvas-wrapper',
+});
+
+// animation loop (not VBCanvas specific)
+(function draw() {
+    // draw a rectangle in the center of the canvas
+    ctx.fillRect(40, 40, 20, 20);
+
+    requestAnimationFrame(draw);
+})();
